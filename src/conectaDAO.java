8312612f28d118dc8b9315d677fdc2016059a323
Oscar Machado <oscarmdas@hotmail.com>
2025-04-03 15:10:45 -0300

    Adição das telas previstas

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -1,13 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Conexao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
 
-/**
- *
- * @author Machados
- */
+
 public class conectaDAO {
+    
+    public Connection connectDB(){
+        Connection conn = null;
+        
+        try {
+        
+            conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost/cinemadb?useSSL=false&serverTimezone=UTC", 
+                "root", 
+                "mySQL121105");
+            
+        } catch (SQLException erro){
+            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+        }
+        return conn;
+    }
     
 }
